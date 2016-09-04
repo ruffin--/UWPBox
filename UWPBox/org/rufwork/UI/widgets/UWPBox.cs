491e6a5dc369a73d37a9e4d67d7f389b566cdc15
Ruffin Bailey <git@rufwork.com>
2016-09-03 20:20:26 -0400

    Trivial: Forgot the ! before the image src/URL in test text.

--- UWPBox/org/rufwork/UI/widgets/UWPBox.cs
+++ UWPBox/org/rufwork/UI/widgets/UWPBox.cs
@@ -805,7 +805,7 @@ namespace org.rufwork.UI.widgets
             {
                 this.Text = @"###Call me Ishmael.
 
-[Jumping Ahead](https://upload.wikimedia.org/wikipedia/commons/7/7b/Moby_Dick_p510_illustration.jpg)
+![Jumping Ahead](https://upload.wikimedia.org/wikipedia/commons/7/7b/Moby_Dick_p510_illustration.jpg)
 
 **Some years ago--*never mind how long precisely*--having little or no money in my purse**, and nothing particular to interest me on shore, I thought I would `sail` about a little and see the watery part of the world. It is a way I have of driving off
 
